d8da972caa24731d931eacda9b63a91c19b20e00
charsyam <charsyam@naver.com>
2020-08-15 01:45:55 +0900

    [ISSUES-#26] get units topology when creating group

--- app/redismon/redismon_manager.py
+++ app/redismon/redismon_manager.py
@@ -1,13 +1,26 @@
-from redismon.model import MonitoringGroup
+from redismon.model import MonitoringGroup, Unit
+from redismon.topology import Topology
+from datetime import datetime
+
 
 class RedisMonManager:
     def __init__(self, db):
         self.db = db
 
     def create_monitoring_group(self, name, seed):
-        group = MonitoringGroup(name, seed)        
+        now = datetime.utcnow()
+        group = MonitoringGroup(name, seed, now)
         self.db.session.add(group)
         self.db.session.commit()
+
+        topology = Topology(seed)
+        for unit in topology.get_topology():
+            addr = "{host}:{port}".format(host=unit["host"], port=unit["port"])
+            unit = Unit(addr, group.id, now)
+            self.db.session.add(unit)
+
+        self.db.session.commit()
+        
         return group
 
     def list_monitoring_groups(self):
