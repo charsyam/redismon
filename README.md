5422544ab410e885d28e0259405fd4f4c4cb2802
charsyam <charsyam@naver.com>
2021-06-30 22:54:01 +0900

    [ISSUE-45] Add Important Stats : connected_clients (#46)
    resolve: #45

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # redismon
 Redis Monitoring Tool
+
+```
+python app.py -a {REDIS_HOST}:{REDIS_PORT}
+```
--- redismon/redis_explainer.py
+++ redismon/redis_explainer.py
@@ -6,10 +6,15 @@ CALLS = "calls"
 CMDSTAT_KEYS = "cmdstat_keys"
 
 
-
-def base_check(cmd, calls, ptime, call_limit, ptime_limit, check_type, description):
+def base_check(name, calls, ptime, call_limit, ptime_limit, check_type, description):
     if ptime >= ptime_limit and calls > call_limit:
-        return {'cmd': cmd, 'value': ptime, 'type': check_type, 'description': description}
+        return {'name': name, 'type': 'cmd', 'value': ptime, 'data_type': check_type, 'description': description}
+
+
+def base_stat_check(name, value, value_limit, description):
+    if value >= value_limit:
+        return {'name': name, 'type': 'stat', 'value': value, 'data_type': 'value', 'description': description}
+        
 
 def explain_slow_cmd(cmd, calls, ptime, call_limit, ptime_limit):
     return base_check(cmd, calls, ptime, call_limit, ptime_limit, PTIME, "some keys have large value")
@@ -30,6 +35,14 @@ def explain_client_cmd(cmd, calls, ptime, call_limit, ptime_limit):
 def explain_cluster_cmd(cmd, calls, ptime, call_limit, ptime_limit):
     return base_check(cmd, calls, ptime, call_limit, ptime_limit, PTIME, "cluster command is basically slow command")
 
+
+def explain_clients_stat(stat, value, value_limit):
+    return base_stat_check(stat, value, value_limit, "connected_clients value is changed a lot")
+
+
+EXPLAIN_STATS_MAP = {
+    "connected_clients":    (explain_clients_stat, 100)
+}
 
 EXPLAIN_CMDS_MAP = {
     "cmdstat_get":      (explain_slow_cmd, 1, 20.0),
@@ -55,7 +68,27 @@ class RedisExplainer:
 
     def explain(self):
         cmds = self.analyze_commands(self.values)
-        return self.explain_commands(cmds)
+        stats = self.analyze_stats(self.values)
+        results = self.explain_commands(cmds)
+        stats_value = self.explain_stats(stats)
+        results.extend(stats_value)
+        return results
+
+    def explain_stats(self, stats):
+        results = []
+        for k, v in stats.items():
+            value = v
+
+            if k in EXPLAIN_STATS_MAP:
+                m = EXPLAIN_STATS_MAP[k]
+                call = m[0]
+                value_limit = m[1]
+
+                ret = call(k, v, value_limit)
+                if ret is not None:
+                    results.append(ret)
+
+        return results
 
     def explain_commands(self, cmds):
         results = []
@@ -87,6 +120,38 @@ class RedisExplainer:
                 cmds.append((key, info[key]))
 
         return cmds
+
+    def get_stats(self, info):
+        stats = []
+
+        for key in info.keys():
+            if key in EXPLAIN_STATS_MAP:
+                stats.append((key, info[key]))
+
+        return stats
+
+    def analyze_stats(self, values):
+        stats = []
+        for value in values:
+            stats.append(self.get_stats(value))
+
+        results = {}
+        for i in range(len(stats) - 1):
+            current_stats = stats[i]
+            next_stats = stats[i+1]
+
+            statMap = {k:v for k, v in current_stats}
+            for stat in next_stats:
+                k = stat[0]
+                v = stat[1]
+
+                if k not in EXPLAIN_STATS_MAP:
+                    continue
+
+                v1 = statMap[k]
+                results[k] = abs(v - v1)
+
+        return results
 
     def analyze_commands(self, values):
         cmds = []
